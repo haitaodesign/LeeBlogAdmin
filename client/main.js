f12f205b37bfe9858490c8d17ea416c9de2df3ab
lihaitao <1775814590@qq.com>
2018-05-22 22:52:49 +0800

    add iview mian.js

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import iView from 'iview'
 import App from './App.vue'
 
 import createRouter from './router/index'
@@ -8,6 +9,7 @@ import './assets/images/bg.jpeg'
 import './assets/styles/test.styl'
 
 Vue.use(VueRouter)
+Vue.use(iView)
 const router = createRouter()
 /* eslint-disable no-new */
 new Vue({
