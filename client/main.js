b7c828dd96ac305814c71075458243065ae4d1f6
lihaitao <1775814590@qq.com>
2018-05-23 17:37:43 +0800

    add iview

--- client/main.js
+++ client/main.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import iView from 'iview'
 import App from './App.vue'
-
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 import createRouter from './router/index'
 
 import './assets/images/bg.jpeg'
 import './assets/styles/test.styl'
-
 Vue.use(VueRouter)
 Vue.use(iView)
 const router = createRouter()
--- client/views/components/header/index.vue
+++ client/views/components/header/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="lee-header">
-    头部
+    <Button>dd</Button>
   </div>
 </template>
 
