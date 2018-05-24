37951217f0c7b51ecd573494c08464f83add2d11
lihaitao <1775814590@qq.com>
2018-05-24 20:19:35 +0800

    update sidebar

--- client/views/components/sidebar/index.vue
+++ client/views/components/sidebar/index.vue
@@ -8,17 +8,21 @@
             </MenuItem>
             <MenuItem name="2">
                 <Icon type="chatbubbles"></Icon>
-                评论管理
+                分类管理
+            </MenuItem>
+            <MenuItem name="3">
+                <Icon type="chatbubbles"></Icon>
+                标签管理
             </MenuItem>
         </MenuGroup>
-        <MenuGroup title="统计分析">
-            <MenuItem name="3">
-                <Icon type="heart"></Icon>
-                用户留存
-            </MenuItem>
+        <MenuGroup title="系统管理">
             <MenuItem name="4">
+                <Icon type="heart"></Icon>
+                用户管理
+            </MenuItem>
+            <MenuItem name="5">
                 <Icon type="heart-broken"></Icon>
-                流失用户
+                个人信息
             </MenuItem>
         </MenuGroup>
     </Menu>
