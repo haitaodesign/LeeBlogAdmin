6c32ac22869e992b9b3ed22d3dddfa69eaff5f08
lihaitao <1775814590@qq.com>
2018-06-03 13:38:23 +0800

    add BaseAxios

---
+++ client/utils/httpClient/index.js
@@ -0,0 +1,31 @@
+import Axios from 'axios'
+import QS from 'qs'
+
+class BaseAxios {
+  constructor () {
+    this.$http = Axios.create({
+      baseURL: 'localhost'
+    })
+    this.OptionsDefault = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      transformRequest: [function (data) {
+        return QS.stringify(data)
+      }]
+    }
+  }
+  get (url, config = {}) {
+    return this.$http.get(url, config)
+  }
+  post (url, data = undefined, config = {}) {
+    return this.$http.post(url, data, {...this.OptionsDefault, ...config})
+  }
+  put (url, data = undefined, config = {}) {
+    return this.$http.put(url, data, {...this.OptionsDefault, ...config})
+  }
+  delete (url, data = undefined, config = {}) {
+    return this.$http.delete(url, config)
+  }
+}
+export default BaseAxios
