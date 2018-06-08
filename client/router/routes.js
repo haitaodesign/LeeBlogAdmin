3b632e98469b2b4e64600335dac977ae2a3c6be5
lihaitao <1775814590@qq.com>
2018-06-08 09:17:44 +0800

    add login route

--- client/router/routes.js
+++ client/router/routes.js
@@ -20,4 +20,8 @@ export default [{
     },
     component: Article
   }]
+}, {
+  path: '/login',
+  name: '登录',
+  component: () => import('../views/login/index.vue')
 }]
--- client/views/login/index.vue
+++ client/views/login/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    登录
+  <div class="lee-login">
+    <div class="lee-login-box">
+
+    </div>
   </div>
 </template>
 
@@ -10,6 +12,15 @@
   }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+.lee-login
+  position absolute;
+  top 0
+  left 0
+  right 0
+  bottom 0
+  background-image url(../../assets/images/login_bg.jpg)
+  background-size 100% 100%
+  background-repeat no-repeat
 </style>
+
