dfd93c652ed9d1d7c99aa6c5ea47140ba7d2baf9
lihaitao <1775814590@qq.com>
2018-06-10 18:16:32 +0800

    add searchForm click

---
+++ client/views/user/components/searchForm/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="searchForm">
+    <Row>
+      <Col span="4">
+        <ButtonGroup>
+         <Button type="primary" @click="_handleAddOnClick">新增</Button>
+         <Button type="primary" @click="_handleUpdateOnClick">修改</Button>
+         <Button type="primary" @click="_handleDeleteOnClick">删除</Button>
+        </ButtonGroup>
+      </Col>
+    </Row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'searchForm',
+    methods: {
+      _handleAddOnClick () {
+        this.$emit('on-add')
+      },
+      _handleUpdateOnClick () {
+        this.$emit('on-update')
+      },
+      _handleDeleteOnClick () {
+        this.$emit('on-delete')
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+.searchForm
+  margin-bottom 10px
+</style>
+
