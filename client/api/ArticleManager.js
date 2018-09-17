f0208cac87cd575ed4b24c24852676aa4b2d3c72
lihaitao <15393243613@163.com>
2018-09-17 18:06:28 +0800

    add ArticleManager API

--- client/api/ArticleManager.js
+++ client/api/ArticleManager.js
@@ -1,7 +1,16 @@
 import BaseAxios from '../utils/httpClient/index'
 class ArticleManager extends BaseAxios {
   getArticleList (page = 0, size = 10) {
-    return this.get(`/users?page=${page}&size=${size}`)
+    return this.get(`/articles?page=${page}&size=${size}`)
+  }
+  add (data) {
+    return this.post('article/add', data)
+  }
+  update (data) {
+    return this.post('article/update', data)
+  }
+  _delete (data) {
+    return this.delete('article/delete' + '/' + data._id)
   }
 }
 
