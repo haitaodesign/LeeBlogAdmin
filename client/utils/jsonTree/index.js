20e8407f3efa1cfdbeabff59dcb844e76928a454
lihaitao <15393243613@163.com>
2018-09-19 21:16:08 +0800

    add 注释

--- client/utils/jsonTree/index.js
+++ client/utils/jsonTree/index.js
@@ -4,6 +4,7 @@ const jsonTree = require('@aximario/json-tree')
  * 返回一个数组对象，包含多个树结构
  * @param {数组，扁平化数据} data
  * @param {配置对象 id，parent_id, children} config
+ * 此方法有个缺陷就是没有对参数进行校验，出错时没有提示，需要优化
  */
 export const getJsonTree = (data, config) => {
   return jsonTree(data, config)
