29c61f0c3604f76623a6af2207d61a5259aafc14
lihaitao <15393243613@163.com>
2018-09-19 21:21:45 +0800

    update 修改菜单数据源

--- client/views/components/sidebar/index.vue
+++ client/views/components/sidebar/index.vue
@@ -11,36 +11,24 @@
 </template>
 
 <script>
-  import { getJsonTree } from '@utils/jsonTree'
-  export default {
-    name: 'sidebar',
-    data () {
-      return {
-        menu: [
-          { id: 1, parent_id: 0, name: '内容管理' },
-          { id: 2, parent_id: 1, name: '文章管理' },
-          { id: 3, parent_id: 1, name: '分类管理' },
-          { id: 4, parent_id: 1, name: '标签管理' },
-          { id: 5, parent_id: 0, name: '系统管理' },
-          { id: 6, parent_id: 5, name: '用户管理' }
-        ]
-      }
-    },
-    computed: {
-      getMenuList () {
-        return getJsonTree(this.menu, {
-          id: 'id',
-          pid: 'parent_id',
-          children: 'children'
-        })
-      }
-    },
-    methods: {
-      _handleOnSelect (name) {
-        this.$emit('on-select', name)
-      }
+import { mapGetters } from 'vuex'
+export default {
+  name: 'sidebar',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapGetters('System', [
+      'getMenuList'
+    ])
+  },
+  methods: {
+    _handleOnSelect (name) {
+      this.$emit('on-select', name)
     }
   }
+}
 </script>
 
 <style lang="stylus" scoped>
