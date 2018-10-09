34b0f70d87d0e734ff652a602a20c0de13371560
lihaitao <15393243613@163.com>
2018-10-09 12:37:33 +0800

    add devtool

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackBar = require('webpackbar')
 module.exports = {
   mode: process.env.NODE_ENV || 'production', // development || production
+  devtool: (process.env.NODE_ENV === 'development') ? 'inline-source-map' : false,
   target: 'web',
   entry: {
     app: path.join(__dirname, '../client/main.js')
