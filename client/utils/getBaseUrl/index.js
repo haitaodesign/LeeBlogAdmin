b6b63880b36923a5683cae403334abf8fad5eb9a
haitaodesign <15393243613@163.com>
2018-12-23 14:35:07 +0800

    fix dev url

--- client/utils/getBaseUrl/index.js
+++ client/utils/getBaseUrl/index.js
@@ -1,22 +1,14 @@
 import config from '../../config'
 const { devUrl, prodUrl } = config
 export default () => {
-  // 开发环境url
-  const devUrls = [{
-    host: 'localhost'
-  }, {
-    host: '0.0.0.0'
-  }]
   // 获取当前浏览器的host
   const host = window.location.host.split(':')[0]
   let currentUrl = '' || devUrl
-  devUrls.forEach((item) => {
-    if (item.host === host) {
-      currentUrl = devUrl
-    } else {
-      currentUrl = prodUrl
-    }
-  })
+  if (host === 'localhost' || '0.0.0.0') {
+    currentUrl = devUrl
+  } else {
+    currentUrl = prodUrl
+  }
   console.info('当前接口地址：', currentUrl)
   return currentUrl
 }
